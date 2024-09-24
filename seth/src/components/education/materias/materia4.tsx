b2f2933ab50076049f78638ffc43e176c051d7a1
Bruno Fernandes Campos <brunofernandescampos@gmail.com>
2024-09-24 18:39:01 -0300

    add Página de Educação
    Adicionei a página de educação completa com os conteúdos e um sistema de progresso.

---
+++ seth/src/components/education/materias/materia4.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../contentEducation.css';
+
+
+const Materia4: React.FC = () => {
+    return (
+        <div className='aulas'>
+<h2>Média Aritmética</h2>
+<p>A média aritmética é uma ferramenta útil para resumir dados, e na meteorologia, podemos usá-la para entender melhor as condições climáticas. Por exemplo, se queremos saber qual foi a temperatura média de uma semana, podemos coletar as temperaturas diárias e calcular a média. Imagine que as temperaturas de uma semana foram: 20°C, 22°C, 25°C, 23°C, 21°C, 24°C e 26°C.</p>
+<p>Para encontrar a média, somamos todas as temperaturas: 20 + 22 + 25 + 23 + 21 + 24 + 26 = 191. Em seguida, dividimos pelo número de dias (7): 191 / 7 ≈ 27,29°C. Assim, podemos dizer que a temperatura média da semana foi de aproximadamente 27°C. Esse cálculo ajuda a entender as tendências climáticas e a nos preparar melhor para o clima que está por vir, permitindo planejar atividades ao ar livre, por exemplo.</p>
+        </div>
+    );
+};
+
+export default Materia4;
