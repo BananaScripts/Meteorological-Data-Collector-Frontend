35929609cf0c379a33987e903f2c8ae744755c89
Gabriel Henrique Siqueira Pinto <gbh.siqueira22@gmail.com>
2024-10-19 16:30:34 -0300

    add: sistema de login incompleto (ref#24)

--- seth/src/components/login/contentLogin.tsx
+++ seth/src/components/login/contentLogin.tsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './contentLogin.css';
+import axios from 'axios';
 
 interface ContentLoginProps {
-    onCloseOtherComponents: () => void;  // Função para fechar outros componentes
+    onCloseOtherComponents: () => void; 
+     // Função para fechar outros componentes
 }
 
-const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) => {
-    const [username, setUsername] = useState('');
+const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents}) => {
+    const [cpf, setCpf] = useState('');
     const [password, setPassword] = useState('');
+
     const [isModalOpen, setIsModalOpen] = useState(false);
+
     const [registerUsername, setRegisterUsername] = useState('');
     const [registerPassword, setRegisterPassword] = useState('');
     const [registerEmail, setRegisterEmail] = useState('');
+    const [registerBirthDate, setRegisterBirthDate] = useState('');
     const [registerCpf, setRegisterCpf] = useState('');
+
     const [isLogin, setIsLogin] = useState(true);
 
     useEffect(() => {
@@ -21,17 +27,48 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
         }
     }, [isModalOpen, onCloseOtherComponents]);
 
-    const handleLogin = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log('Username:', username);
-        console.log('Password:', password);
+    const handleLogin = async() => {
+        try{
+            const response = await axios.post('http://localhost:3002/api/login', {cpf: cpf, senha: password})
+            if (response.status === 202){
+                localStorage.setItem("token", response.data.token)
+                alert('Login bem-sucedido')
+            }
+            else{
+                alert("Erro ao efeturar o login")
+            }
+        }
+        catch(error){
+            console.error(error)
+        }
     };
 
-    const handleRegister = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log('Register Username:', registerUsername);
-        console.log('Register Password:', registerPassword);
-        console.log('Register Email:', registerEmail);
+    const handleRegister = async() => {
+        try{
+            if(registerUsername !== '' && registerBirthDate !== '' && registerCpf !== '' && registerEmail !== ''&& registerPassword !== ''){
+                axios.post('localhost:3002/api/usuario/cadastrar', {registerUsername, registerBirthDate, registerCpf ,registerEmail, registerPassword})
+                .then(()=>{
+                    setRegisterUsername('');
+                    setRegisterEmail('');
+                    setRegisterBirthDate('');
+                    setRegisterCpf('');
+                    setRegisterPassword('')
+                    alert('Credenciais cadastradas com sucesso!')
+                    setIsLogin(true)
+                })
+                .catch((error)=>{
+                    console.error(error)
+                    alert('Erro ao registrar credenciais.')
+                })
+            }
+            else{
+                /* VAI QUE */
+                alert("Preencha todos os campos.")
+            }
+        }
+        catch(erro){
+            console.error(erro);
+        } 
     };
 
     const openModal = () => {
@@ -46,6 +83,12 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
         setIsLogin(!isLogin);
     };
 
+    /* Função de teste */
+    const logout = () =>{
+        localStorage.removeItem("token");
+        alert("Usuario deslogado.")
+    }
+
     return (
         <>
             <button onClick={openModal} className='button-Login'>.</button>
@@ -57,15 +100,16 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
                             {isLogin ? (
                                 <div className="form-section">
                                     <h2>Login</h2>
-                                    <form onSubmit={handleLogin}>
+        
                                         <div>
-                                            <label htmlFor="username">Username:</label>
+                                            <label htmlFor="cpf">CPF:</label>
                                             <input
                                                 type="text"
-                                                id="username"
+                                                id="cpf"
                                                 className="inputLogin"
-                                                value={username}
-                                                onChange={(e) => setUsername(e.target.value)}
+                                                value={cpf}
+                                                onChange={(e) => setCpf(e.target.value)}
+                                                maxLength={11}
                                                 required
                                             />
                                         </div>
@@ -80,11 +124,12 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
                                                 required
                                             />
                                         </div>
-                                        <button type="submit" className="submitButton">Login</button>
+                                        <button className="submitButton" onClick={handleLogin}>Login</button>
+                                        <button className="toggle-button" onClick={logout}>Logout</button>
                                         <button className="toggle-button" onClick={toggleForm}>
                                             {isLogin ? 'Switch to Cadastro' : 'Switch to Login'}
                                         </button>
-                                    </form>
+                                    
                                 </div>
                             ) : (
                                 <div className="form-section">
@@ -113,6 +158,17 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
                                             />
                                         </div>
                                         <div>
+                                            <label htmlFor="registerBirthDate">Data de nascimento:</label>
+                                            <input
+                                                type="date"
+                                                id="registerBirthDate"
+                                                className="inputCadastro"
+                                                value={registerBirthDate}
+                                                onChange={(e) => setRegisterBirthDate(e.target.value)}
+                                                required
+                                            />
+                                        </div>
+                                        <div>
                                             <label htmlFor="registerEmail">Email:</label>
                                             <input
                                                 type="email"
@@ -131,10 +187,11 @@ const ContentLogin: React.FC<ContentLoginProps> = ({ onCloseOtherComponents }) =
                                                 className="inputCadastro"
                                                 value={registerCpf}
                                                 onChange={(e) => setRegisterCpf(e.target.value)}
+                                                maxLength={11}
                                                 required
                                             />
                                         </div>
-                                        <button type="submit" className="submitButton">Register</button>
+                                        <button type="submit" className="submitButton" onClick={handleRegister}>Register</button>
                                         <button className="toggle-button" onClick={toggleForm}>
                                             {isLogin ? 'Switch to Cadastro' : 'Switch to Login'}
                                         </button>
